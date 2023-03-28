43f71532f739f4bdff13bc22b6e0c5bd8ab30254
Alex Tsimba <simba292@gmail.com>
2023-03-28 12:58:47 +0300

    cleanup template

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-ts-tailwind-boilerplate',
+  base: '/todo-firebase',
   plugins: [react()],
 });
