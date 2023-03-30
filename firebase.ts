12e7f6dccae33c652036660596963feb2002ed30
Alex Tsimba <simba292@gmail.com>
2023-03-30 20:33:08 +0300

    implement drag-n-drop

--- firebase.ts
+++ firebase.ts
@@ -1,16 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyA_HlO28q0In0rowj0tZsHuiTKnWu2GOHE',
-//   authDomain: 'todo-app-9e93a.firebaseapp.com',
-//   projectId: 'todo-app-9e93a',
-//   storageBucket: 'todo-app-9e93a.appspot.com',
-//   messagingSenderId: '575784870479',
-//   appId: '1:575784870479:web:b14ca15276e00f28e1c03a',
-//   measurementId: 'G-BX8S78MEQW',
-// };
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
   authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
