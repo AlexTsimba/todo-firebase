1ddb554a1f5eefdf851fe0d651c83c91551ed563
Alex Tsimba <simba292@gmail.com>
2023-03-31 16:29:51 +0300

    rework: split components structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,35 @@
-import { Reorder } from 'framer-motion';
 import { useEffect, useState } from 'react';
-import { AiOutlinePlus } from 'react-icons/ai';
-import TodoItem from './components/TodoItem/TodoItem';
+import AddTodo from './components/AddTodo/AddTodo';
+import TodoList from './components/TodoList/TodoList';
 import { Todo } from './Types/Todo';
-import {
-  fetchTodos,
-  deleteTodo,
-  addTodo,
-  updateTodosOrder,
-} from './utils/Utils';
+import { fetchTodos } from './utils/Utils';
 
 const style = {
   bg: `min-h-screen w-screen overflow-hidden p-4 bg-gradient-to-r from-blue-300 to-blue-400`,
   container: `bg-slate-100 rounded-lg p-6 shadow-xl max-w-[1024px] m-auto`,
   heading: `text-3xl font-bold text-center text-gray-700 mb-2`,
-  form: `flex justify-between gap-x-2`,
-  input: `w-full border pl-6`,
-  button: `border rounded-sm p-2 bg-slate-200 hover:bg-slate-300`,
   counter: `text-center`,
 };
 
 export default function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [input, setInput] = useState<string>('');
-  const [processing, setProcessing] = useState<boolean>(false);
+  const [isAdding, setIsAdding] = useState<boolean>(false);
 
   useEffect(() => {
     fetchTodos(setTodos);
-    setProcessing(false);
-  }, [processing, setTodos]);
 
-  const handlInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(event.target.value);
-  };
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    if (input.length > 0) {
-      const newTodo = {
-        name: input,
-        completed: false,
-        order: todos.length,
-      };
-
-      addTodo(newTodo);
-      setProcessing(true);
-      setInput('');
-    }
-  };
-
-  const HandleDeleteTodo = async (todo: Todo) => {
-    deleteTodo(todo);
-    setTodos((prevTodos) => prevTodos.filter((t) => t.id !== todo.id));
-  };
-
-  const handleReorder = (reorderedTodos: Todo[]) => {
-    const updatedTodos = reorderedTodos.map((todo, index) => {
-      return {
-        ...todo,
-        order: index,
-      };
-    });
-    setTodos(updatedTodos);
-    updateTodosOrder(updatedTodos);
-  };
+    setIsAdding(false);
+  }, [isAdding]);
 
   return (
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>Todo App</h3>
-        <form className={style.form} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            className={style.input}
-            placeholder="Add Todo"
-            value={input}
-            onChange={handlInputChange}
-            disabled={processing}
-          />
-          <button className={style.button}>
-            <AiOutlinePlus size={30} />
-          </button>
-        </form>
-        <Reorder.Group values={todos} onReorder={handleReorder}>
-          {todos.map((todo) => {
-            return (
-              <TodoItem todo={todo} key={todo.id} onDelete={HandleDeleteTodo} />
-            );
-          })}
-        </Reorder.Group>
+
+        <AddTodo todosLength={todos.length} setIsAdding={setIsAdding} />
+
+        <TodoList todos={todos} setTodos={setTodos} />
+
         {todos.length > 0 && (
           <p className={style.counter}>You have {todos.length} todos left</p>
         )}
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Reorder } from 'framer-motion';
+import { Todo } from '../../Types/Todo';
+import TodoItem from '../TodoItem/TodoItem';
+import { deleteTodo, updateTodosOrder } from '../../utils/Utils';
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+  const handleDeleteTodo = async (todo: Todo) => {
+    deleteTodo(todo);
+    setTodos((prevTodos) => prevTodos.filter((t) => t.id !== todo.id));
+  };
+
+  const handleReorder = (reorderedTodos: Todo[]) => {
+    const updatedTodos = reorderedTodos.map((todo, index) => {
+      return {
+        ...todo,
+        order: index,
+      };
+    });
+    setTodos(updatedTodos);
+    updateTodosOrder(updatedTodos);
+  };
+
+  return (
+    <Reorder.Group values={todos} onReorder={handleReorder}>
+      {todos.map((todo) => {
+        return (
+          <TodoItem todo={todo} key={todo.id} onDelete={handleDeleteTodo} />
+        );
+      })}
+    </Reorder.Group>
+  );
+};
+
+export default TodoList;
